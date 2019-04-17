2565faed190e49848ffd5ca6f90a8049cf8e3af8
DimaZabavskiy <DimaZabavskiy@yandex.ru>
2019-04-17 08:57:26 +0300

    making rainBow

---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+    var result;
+    var divItem;
+    for (var i = 0; i <this.props.colors.length; i++ ) {
+      if (i == 0) {
+        divItem = this.props.children;
+      }
+      result = <div style={{border:"solid 5px "+this.props.colors[i],padding:"5px", textAlign: "center"}}>
+        {divItem}
+      </div>
+      divItem = result;
+    }
+
+    return (    
+      result
+    );
+  }
+
+}
+
+export default RainbowFrame;
