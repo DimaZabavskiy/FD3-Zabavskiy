d3bb6626ba1c39f8684398b2c54ce6d483c6ff47
DimaZabavskiy <DimaZabavskiy@yandex.ru>
2019-05-05 16:02:25 +0300

    HOC

---
+++ RainbowFrameHOC/components/Rainbow.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import { Fragment} from 'react';
+
+import './Rainbow.css';
+
+import { withRainbowFrame } from './withRainbowFrame';
+
+class Rainbow extends React.Component {
+  render() {
+    let colors=['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+    let FramedFragment=withRainbowFrame(colors)(Fragment);
+    return (
+      <FramedFragment>
+        Hello!
+      </FramedFragment>
+    );
+  }
+}
+
+export default Rainbow;
---
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+
+function withRainbowFrame(wrapper) {
+  return function(Component) {
+    return props => {
+      var result;
+      var divItem;
+      for (var i = 0; i <wrapper.length; i++ ) {
+        if (i == 0) {
+          divItem = <Component {...props} />;
+        }
+        result = <div style={{border:"solid 5px "+ wrapper[i],padding:"5px", textAlign: "center"}}>
+          {divItem}
+        </div>
+        divItem = result;
+      }
+      return result
+    };
+  };
+}
+
+export { withRainbowFrame };
