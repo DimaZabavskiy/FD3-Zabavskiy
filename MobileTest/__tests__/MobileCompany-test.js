2b4429e1e695dbd8d19bccf1c07d1389162906d6
DimaZabavskiy <DimaZabavskiy@yandex.ru>
2019-05-16 22:28:51 +0300

    mobile_test

---
+++ MobileTest/__tests__/MobileCompany-test.js
@@ -0,0 +1,92 @@
+﻿"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MobileCompany from '../components/MobileCompany';
+import MobileClient from '../components/MobileClient';
+
+test('работа MobileCompany', () => {
+  let clientsArr=[ 
+    {id:101, fam:"Иванов", im:"Иван", otch:"Иванович", balance:200}, 
+    {id:105, fam:"Сидоров", im:"Сидор", otch:"Сидорович", balance:-250}, 
+    {id:110, fam:"Петров", im:"Пётр", otch:"Петрович", balance:180},
+    {id:120, fam:"Григорьев", im:"Григорий", otch:"Григорьевич", balance:-220},
+  ];
+
+  // создаём тестовую версию компонента
+  const component = renderer.create(
+    <MobileCompany clients={clientsArr}/>
+  );
+
+  // получаем снэпшот (HTML-снимок) компонента для сверки, что вёрстка не испортилась
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента кнопку 'Все'
+  const buttonElem = component.root.find( el => el.type=='input' && el.props.value == 'Все' ); 
+  buttonElem.props.onClick();
+
+  // получаем уже изменённый снэпшот
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  
+  // найдём в вёрстке компонента кнопку 'Активные'
+  const buttonElemActive = component.root.find( el => el.type=='input' && el.props.value == 'Активные' ); 
+  buttonElemActive.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента кнопку 'Заблокированные'
+  const buttonElemBlocked = component.root.find( el => el.type=='input' && el.props.value == 'Заблокированные' ); 
+  buttonElemBlocked.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  const component2 = renderer.create(
+    <MobileClient key={clientsArr[1].id} info={clientsArr[1]}/>
+  );
+
+  // найдём в вёрстке компонента кнопку 'Удалить'
+  const buttonDelete = component2.root.find( el => el.type=='input' && el.props.value == 'Удалить' );
+  buttonDelete.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  const component3 = renderer.create(
+    <MobileClient key={clientsArr[3].id} info={clientsArr[3]}/>
+  );
+
+  // найдём в вёрстке компонента кнопку 'Редактировать'
+  const buttonCorrect = component3.root.find( el => el.type=='input' && el.props.value == 'Редактировать' );
+  buttonCorrect.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента кнопку 'Добавить изменения'
+  const buttonElemChange = component.root.find( el => el.type=='input' && el.props.value == 'Добавить' ); 
+  buttonElemChange.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента кнопку 'Добавить клиента'
+  const buttonElemAdd = component.root.find( el => el.type=='input' && el.props.value == 'Добавить клиента' ); 
+  buttonElemAdd.props.onClick();
+  
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+  // найдём в вёрстке компонента кнопку 'Отмена'
+  const buttonElemCancel = component.root.find( el => el.type=='input' && el.props.value == 'Отмена' ); 
+  buttonElemCancel.props.onClick();
+
+  componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+  
+});
---
+++ MobileTest/components/MobileClient.js
@@ -0,0 +1,56 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import {voteEvents} from './events';
+
+//import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+
+  state = {
+    info: this.props.info,
+  };
+
+  deleteClient = () => {
+    voteEvents.emit('deleteClient',this.props.info.id);
+  }
+  correct = () => {
+    voteEvents.emit('correct',this.props.info);
+  }
+
+  componentWillReceiveProps = (newProps) => {
+    this.setState({info:newProps.info});
+  };
+
+  render() {
+
+    //console.log("MobileClient id="+this.state.info.id+" render");
+
+    let status;
+    if (this.state.info.balance >= 0) {
+      status = 'active';
+    } else {
+      status = 'blocked';
+    }
+    
+    return (      
+      <tr>
+        <td>{this.state.info.fam}</td> 
+        <td>{this.state.info.im}</td> 
+        <td>{this.state.info.otch}</td> 
+        <td>{this.state.info.balance}</td> 
+        <td className={status}>{status}</td> 
+        <td>
+          <input type="button" value="Редактировать" onClick={this.correct} />
+        </td> 
+        <td>
+          <input type="button" value="Удалить" onClick={this.deleteClient} />
+        </td> 
+      </tr>
+    );
+
+  }
+
+}
+
+export default MobileClient;
