ae4d4265ddcbfcf5ccc2cf750348900448160944
DimaZabavskiy <DimaZabavskiy@yandex.ru>
2019-05-26 09:58:21 +0300

    firstRemake

--- Scales/app.js
+++ Scales/app.js
@@ -21,14 +21,14 @@ var Scales = /** @class */ (function () {
     Scales.prototype.getSumScale = function () {
         var sum = 0;
         for (var i = 0; i < this.addedProducts.length; i++) {
-            sum += this.addedProducts[i].scale;
+            sum += this.addedProducts[i].getScale();
         }
         console.log('Общий вес продуктов на весах: ' + sum);
     };
     Scales.prototype.getNameList = function () {
         var allName = [];
         for (var i = 0; i < this.addedProducts.length; i++) {
-            allName.push(this.addedProducts[i].name);
+            allName.push(this.addedProducts[i].getName());
         }
         console.log("Перечень всех продуктов на весах: " + allName);
     };
@@ -40,10 +40,10 @@ var Product = /** @class */ (function () {
         this.scale = _scale;
     }
     Product.prototype.getScale = function () {
-        console.log('Вес продукта ' + this.name + ': ' + this.scale);
+        return this.scale;
     };
     Product.prototype.getName = function () {
-        console.log("Название продукта: " + this.name);
+        return this.name;
     };
     return Product;
 }());
@@ -55,14 +55,6 @@ var Apple = /** @class */ (function (_super) {
         Apple.applesNames.push(_this.name);
         return _this;
     }
-    Apple.prototype.getScale = function () {
-        _super.prototype.getScale.call(this);
-        console.log('Вес всех Apples: ' + Apple.applesScale);
-    };
-    Apple.prototype.getName = function () {
-        _super.prototype.getName.call(this);
-        console.log("Все сорты Apples: " + Apple.applesNames);
-    };
     Apple.applesScale = 0;
     Apple.applesNames = [];
     return Apple;
@@ -75,31 +67,16 @@ var Tomato = /** @class */ (function (_super) {
         Tomato.tomatoesNames.push(_this.name);
         return _this;
     }
-    Tomato.prototype.getScale = function () {
-        _super.prototype.getScale.call(this);
-        console.log('Вес всех Tomatoes: ' + Tomato.tomatoesScale);
-    };
-    Tomato.prototype.getName = function () {
-        _super.prototype.getName.call(this);
-        console.log("Все сорты Tomatoes: " + Tomato.tomatoesNames);
-    };
     Tomato.tomatoesScale = 0;
     Tomato.tomatoesNames = [];
     return Tomato;
 }(Product));
-console.log('Накинем яблочек:');
 var apple1 = new Apple('Северный синап', 300);
 var apple2 = new Apple('Мельба', 100);
 var apple3 = new Apple('Богатырь', 500);
-apple3.getScale();
-apple3.getName();
-console.log('А теперь время томата:');
 var tomato1 = new Tomato('Снежный барс', 200);
 var tomato2 = new Tomato('Гаспачо', 600);
 var tomato3 = new Tomato('Даренка', 400);
-tomato3.getScale();
-tomato3.getName();
-console.log('Итого на весах:');
 var scale = new Scales();
 scale.add(apple1);
 scale.add(apple2);
--- Scales/app.ts
+++ Scales/app.ts
@@ -1,20 +1,20 @@
 
 class Scales {
 
-    addedProducts:Array<{name:string,scale:number}>;
+    addedProducts:Array<Product>;
 
     constructor() {
         this.addedProducts= [];
     }
 
-    add(prod:{name:string,scale:number}):void {
+    add(prod: Product):void {
         this.addedProducts.push(prod);
     }
 
     getSumScale():void {
         let sum:number = 0;
         for (let i:number = 0; i < this.addedProducts.length; i++) {
-            sum += this.addedProducts[i].scale;
+            sum += this.addedProducts[i].getScale();
         }
         console.log('Общий вес продуктов на весах: ' + sum);
     }
@@ -22,7 +22,7 @@ class Scales {
     getNameList():void {
         let allName:Array<string> = [];
         for (let i:number = 0; i < this.addedProducts.length; i++) {
-            allName.push(this.addedProducts[i].name);
+            allName.push(this.addedProducts[i].getName());
         }
         console.log("Перечень всех продуктов на весах: " + allName);
     }
@@ -38,13 +38,13 @@ class Product {
         this.name = _name;
         this.scale = _scale;
     }
-    
-    getScale():void {
-        console.log('Вес продукта ' + this.name + ': ' + this.scale);
+
+    getScale():number {
+        return this.scale;
     }
 
-    getName():void {
-        console.log("Название продукта: " + this.name);
+    getName():string {
+        return this.name;
     }
 
 }
@@ -59,16 +59,6 @@ class Apple extends Product {
         Apple.applesScale += this.scale;
         Apple.applesNames.push(this.name);
     }
-    
-    getScale():void {
-        super.getScale();
-        console.log('Вес всех Apples: ' + Apple.applesScale);
-    }
-    
-    getName():void {
-        super.getName();
-        console.log("Все сорты Apples: " + Apple.applesNames);
-    }
 
 }
 
@@ -82,34 +72,17 @@ class Tomato extends Product {
         Tomato.tomatoesScale += this.scale;
         Tomato.tomatoesNames.push(this.name);
     }
-    
-    getScale():void {
-        super.getScale();
-        console.log('Вес всех Tomatoes: ' + Tomato.tomatoesScale);
-    }
-    
-    getName():void {
-        super.getName();
-        console.log("Все сорты Tomatoes: " + Tomato.tomatoesNames);
-    }
 
 }
 
-console.log('Накинем яблочек:');
 let apple1:Apple = new Apple ('Северный синап', 300);
 let apple2:Apple = new Apple ('Мельба', 100);
 let apple3:Apple = new Apple ('Богатырь', 500);
-apple3.getScale();
-apple3.getName();
 
-console.log('А теперь время томата:');
 let tomato1:Apple = new Tomato ('Снежный барс', 200);
 let tomato2:Apple = new Tomato ('Гаспачо', 600);
 let tomato3:Apple = new Tomato ('Даренка', 400);
-tomato3.getScale();
-tomato3.getName();
 
-console.log('Итого на весах:')
 let scale: Scales = new Scales();
 scale.add(apple1);
 scale.add(apple2);
